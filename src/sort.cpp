3be97fee9833b09fa520ec9b70e76f27a1c9283f
sillverskui <testdas@mail.ru>
2025-10-15 17:48:07 +0300

    updated sort

--- src/sort.cpp
+++ src/sort.cpp
@@ -2,5 +2,26 @@
 
 void Sort(std::list<int>& nums)
 {
-    nums.sort(greater<int>());
+    if (nums.size() < 2) return;
+
+    bool sorted;
+    do
+    {
+        sorted = false;
+
+        auto it = nums.begin();
+        while (it != nums.end())
+        {
+            auto next = it;
+            ++next;
+            if (next == nums.end()) break;
+
+            if (*it < *next)
+            {
+                iter_swap(next, it);
+                sorted = true;
+            }
+            ++it;
+        }
+    } while (sorted);
 }
